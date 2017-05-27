ce50ec29766a6139f25e14d86c5a8020f97e706b
mbusso <matiassbb@gmail.com>
2017-05-27 18:43:11 -0300

    get cantidadates info for all pages

--- scraping/politicosOnLine.py
+++ scraping/politicosOnLine.py
@@ -3,33 +3,48 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-r  = requests.get("http://www.politicosonline.com/ranking")
-r.encoding = 'utf-8'
-data = r.text.encode('utf-8')
+def main():
+	soup = get_content("http://www.politicosonline.com/ranking")
+	paginationNumbers = get_pagination_numbers(soup)
+	candidates = parse_candidates(soup)	
 
-soup = BeautifulSoup(data, 'html.parser')
+	for number in paginationNumbers:
+		url = "http://www.politicosonline.com/ranking?desde={}&orderby=klout".format(int(number) - 1)
+		print url
+		candidates.extend(parse_candidates(get_content(url)))
 
-paginationBlock = soup.find(id="pagination-digg").find_all("a")
+	with open('data.json', 'w') as outfile:
+	    json.dump(candidates, outfile, ensure_ascii=False)
 
-def paginationNumbers(x): return str(x.text)
 
-paginationNumbers = map(paginationNumbers, paginationBlock[0:5])
-contents = soup.find(id="klout-top").find_all("tr")
+def parse_candidates(soup):
+	contents = soup.find(id="klout-top").find_all("tr")
+	candidates = []
+	for tr in contents[1: len(contents) -1]:
+		tds = tr.find_all("td")
+		data = {}
+		data['img'] = tr.find_all("td")[0].contents[0]["src"]
+		data['name'] = tr.find_all("td")[3].contents[0].text.encode('utf-8')
+		data['href'] = tr.find_all("td")[3].contents[0]["href"]
+		data['amountOfFollowers'] = tr.find_all("td")[5].text.encode('utf-8')
+		data['description'] = str(tr.find_all("td")[6].text.encode('utf-8'))
+		candidates.append(data)
+	return candidates
 
-candidates = []
-for tr in contents[1: len(contents) -1]:
-	tds = tr.find_all("td")
-	data = {}
-	data['img'] = tr.find_all("td")[0].contents[0]["src"]
-	data['name'] = tr.find_all("td")[3].contents[0].text.encode('utf-8')
-	data['href'] = tr.find_all("td")[3].contents[0]["href"]
-	data['amountOfFollowers'] = tr.find_all("td")[5].text.encode('utf-8')
-	data['description'] = str(tr.find_all("td")[6].text.encode('utf-8'))
-	candidates.append(data)
+def get_content(url):
+	r  = requests.get(url)
+	r.encoding = 'utf-8'
+	data = r.text.encode('utf-8')
+	return BeautifulSoup(data, 'html.parser')
 
-with open('data.json', 'w') as outfile:
-    json.dump(candidates, outfile, ensure_ascii=False)
-	
+def get_pagination_numbers(soup):
+	paginationBlock = soup.find(id="pagination-digg").find_all("a")
+	def paginationNumbers(x): return str(x.text)
+	return map(paginationNumbers, paginationBlock[0:5])
+
+if __name__ == "__main__":
+    main()
+
 
 #http://www.politicosonline.com/ranking?desde=250&orderby=klout
 #sudo apt-get install python-pip
