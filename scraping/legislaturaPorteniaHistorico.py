88040e35077e40badd256c7d3571f97b2fc466d0
mbusso <matiassbb@gmail.com>
2017-05-29 00:52:09 -0300

    make request to retrieve more info about candidate

--- scraping/legislaturaPorteniaHistorico.py
+++ scraping/legislaturaPorteniaHistorico.py
@@ -1,16 +1,14 @@
 import xml.etree.ElementTree as ET
 import unicodedata
+import requests
 import json
-
 
 def main():
 	candidateSurname = u"Sanchez Andia"
 	tree = ET.parse('GetDiputadosHistorico.xml')
 	historicalCandidate = findHistoricalCandidate(tree.getroot(),candidateSurname)
-	print historicalCandidate
-	
-
-
+	if(historicalCandidate):
+		findInfo(historicalCandidate["id_legislador"]) #TODO: PARSE DATA
 
 def findHistoricalCandidate(root, surname):
 	results = []
@@ -29,11 +27,17 @@ def findHistoricalCandidate(root, surname):
 	if(len(results) == 0) :
 		return {}
 
-	return json.dumps(results[0], ensure_ascii=False)
+	return results[0]
 
 def normalize(string):
 	return unicodedata.normalize('NFKD', unicode(string)).encode('ASCII', 'ignore').lower()
 
+
+def findInfo(legisladorId):
+	payload = "id_legislador={}".format(legisladorId)
+	headers = {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+	r = requests.post("https://parlamentaria.legislatura.gov.ar/webservices/Json.asmx/GetDiputadosyCargosActivosPorId_Legislador", payload, headers=headers)
+	print r.text
 
 if __name__ == "__main__":
     main()
