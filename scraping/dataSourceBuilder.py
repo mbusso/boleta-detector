853b33c412af541bfb06c5309acc3a878029939d
mbusso <mbusso@hexacta.com>
2017-06-05 16:56:33 -0300

    create a dict with the current sources

--- scraping/dataSourceBuilder.py
+++ scraping/dataSourceBuilder.py
@@ -3,39 +3,43 @@ import base64
 import requests
 
 def main():
-	with open('boletas.json') as json_data:
-	    boletas = json.load(json_data)
-	    for boleta in boletas:
-	    	process(boleta)
+	boletas = readJsonFile('boletas.json')
+	source = {}
+	source["diputados"] = readJsonFile('diputados.json')
+	source["senadores"] = readJsonFile('senadores.json')
+	for boleta in boletas:
+	  	process(boleta, source)
 	    
 
-def process(boleta):
+def process(boleta, source):
 	for candidate in boleta["candidatos"]:
-		diputadoData = findInDiputados(candidate)
-		senadorData = findInSenadores(candidate)
+		diputadoData = findInDiputados(candidate, source["diputados"])
+		senadorData = findInSenadores(candidate, source["senadores"])
 
-def findInDiputados(candidate):
+def findInDiputados(candidate, sources):
 	results = []
-	with open('diputados.json') as diputados_data:
-		diputados = json.load(diputados_data)
-		for diputado in diputados:
-			if(candidate["apellido"] == diputado["apellido"]):
-				diputado["imgAsb64"] = getImgAsBase64(diputado["img"])
-				results.append(diputado)
+	for diputado in sources:
+		if(candidate["apellido"] == diputado["apellido"]):
+			diputado["imgAsb64"] = getImgAsBase64(diputado["img"])
+			results.append(diputado)
 	return results
 
-def findInSenadores(candidate):
+def findInSenadores(candidate, sources):
 	results = []
-	with open('senadores.json') as senadores_data:
-		senadores = json.load(senadores_data)
-		for senador in senadores:
-			if(candidate["apellido"] in senador["nombre"]):
-				senador["imgAsb64"] = getImgAsBase64(senador["img"])
-				results.append(senador)
+	for senador in sources:
+		if(candidate["apellido"] in senador["nombre"]):
+			senador["imgAsb64"] = getImgAsBase64(senador["img"])
+			results.append(senador)
 	return results
 
 def getImgAsBase64(url):
 	return base64.b64encode(requests.get(url).content)
+
+def readJsonFile(name):
+	f = open(name, 'r') 
+	data = json.load(f)
+	f.close()
+	return data
 
 if __name__ == "__main__":
     main()
