ec5e82c74fd570e740c3afc1dfec059d707d5100
mbusso <matiassbb@gmail.com>
2017-06-25 16:20:17 -0300

    add full img path for precandidates

--- scraping/precandidatosPaso.py
+++ scraping/precandidatosPaso.py
@@ -5,7 +5,13 @@ import json
 
 def main():
 	data = request.get_content("http://olcreativa.lanacion.com.ar/dev/get_url/?key2=1mIchf9frOP7w9IJfqIC7QJ28VB0SwwfAB2bJqP4AaBM&gid=0&output=json")
-	files.save_as_json_2('sources/paso2017.json', json.loads(data))
+	jsonOutput = process(data)
+	files.save_as_json_2('sources/paso2017.json', jsonOutput)
 
+def process(result):
+	data = json.loads(result)
+	for candidate in data:
+		candidate["img"] = "http://especiales.lanacion.com.ar/multimedia/proyectos/17/elecciones/elecciones_2017_listas_confirmadas/img/" + candidate["img"]
+	return data
 if __name__ == "__main__":
     main()
