970b2d0d6b368fa5877c5e9689fc08f91290db97
mbusso <mbusso@hexacta.com>
2017-06-26 11:19:07 -0300

    remove undefined candidates

--- scraping/createBoletasSource.py
+++ scraping/createBoletasSource.py
@@ -12,17 +12,18 @@ def map(precandidates):
 	result = {}
 	for precandidate in precandidates:
 		data = {}
-		data["partido"] = precandidate["nombre_partido"]
-		data["nombre"] = re.sub(r'\d+-\s', '', precandidate["nombre"])
-		data["apellido"] = precandidate["apellido"]
-		data["img"] = precandidate["img"]
-		data["banca"] = precandidate["banca"]
-		data["distrito"] = precandidate["distrito"]
+		if(precandidate["apellido"].lower() != "definir"):
+			data["partido"] = precandidate["nombre_partido"]
+			data["nombre"] = re.sub(r'\d+-\s', '', precandidate["nombre"])
+			data["apellido"] = precandidate["apellido"] 
+			data["img"] = precandidate["img"]
+			data["banca"] = precandidate["banca"]
+			data["distrito"] = precandidate["distrito"]
 
-		if(data["partido"] in result):
-			result[data["partido"]].append(data)
-		else :
-			result[data["partido"]] = [data]
+			if(data["partido"] in result):
+				result[data["partido"]].append(data)
+			else :
+				result[data["partido"]] = [data]
 
 	output = []
 	for key in result:
