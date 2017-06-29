7aac64830345d487bff141f2f3701ce720510759
mbusso <matiassbb@gmail.com>
2017-06-29 00:01:32 -0300

    candidate generalization for search results

--- scraping/duckduckgo.py
+++ scraping/duckduckgo.py
@@ -3,8 +3,25 @@ from modules import files
 import urllib
 
 def main():
-    soup = request.get_content_parsed("https://duckduckgo.com/html/?q=liliana+fadul+tierra+del+fuego")
-    files.save_as_json_2('sources/resultsGoGoDuck.json', parse_results(soup))
+    candidate = findCandidateResources("liliana", "fadul", "tierra del fuego")
+    results = []
+    results.append(candidate)
+    files.save_as_json_2('sources/resultsGoGoDuck.json', results)
+
+
+def findCandidateResources(name, surname, province):
+    nameFormatted = name.replace(" ", "+")
+    surnameFormatted = surname.replace(" ", "+")
+    provinceFormatted = province.replace(" ", "+")
+    url = "https://duckduckgo.com/html/?q={}+{}+{}".format(nameFormatted, surnameFormatted, provinceFormatted)
+    results = parse_results(request.get_content_parsed(url))
+    candidate = {}
+    candidate["nombre"] = name
+    candidate["apellido"] = surname
+    candidate["distrito"] = province
+    candidate["url"] = url
+    candidate["resources"] = results
+    return candidate
 
 def parse_results(soup):
     resultsBlocks = soup.find_all("div", attrs={"class":"links_main links_deep result__body"})
