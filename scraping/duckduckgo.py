6762a81a92472aee6470b61bbe0b63973ba3bd68
mbusso <matiassbb@gmail.com>
2017-06-29 00:25:09 -0300

    first step to integrate search candidate resources process

--- scraping/duckduckgo.py
+++ scraping/duckduckgo.py
@@ -1,12 +1,18 @@
 from modules import request
 from modules import files
+from modules import repository
 import urllib
+import time
 
 def main():
-    candidate = findCandidateResources("liliana", "fadul", "tierra del fuego")
-    results = []
-    results.append(candidate)
-    files.save_as_json_2('sources/resultsGoGoDuck.json', results)
+    candidates = repository.findCandidatesWithoutResources()
+    print candidates
+
+    #candidate = findCandidateResources("liliana", "fadul", "tierra del fuego")
+    #results = []
+    #results.append(candidate)
+    #files.save_as_json_2('sources/resultsGoGoDuck.json', results)
+    #time.sleep(5)   # delays for 5 seconds. You can Also Use Float Value.
 
 
 def findCandidateResources(name, surname, province):
@@ -24,15 +30,19 @@ def findCandidateResources(name, surname, province):
     return candidate
 
 def parse_results(soup):
-    resultsBlocks = soup.find_all("div", attrs={"class":"links_main links_deep result__body"})
     results = []
-    for resultDiv in resultsBlocks:
-        data = {}
-        url = resultDiv.find("a", attrs={"class":"result__a"})["href"]
-        data["url"] = urllib.unquote(url.split("uddg=")[1])
-        data["description"] = resultDiv.find("a", attrs={"class":"result__snippet"}).text
-        results.append(data)
-    return results
+    resultsBlocks = soup.find_all("div", attrs={"class":"links_main links_deep result__body"})
+    if(len(resultsBlocks) == 0):
+        print soup
+        return results
+    else:    
+        for resultDiv in resultsBlocks:
+            data = {}
+            url = resultDiv.find("a", attrs={"class":"result__a"})["href"]
+            data["url"] = urllib.unquote(url.split("uddg=")[1])
+            data["description"] = resultDiv.find("a", attrs={"class":"result__snippet"}).text
+            results.append(data)
+        return results
 
 if __name__ == "__main__":
     main()
